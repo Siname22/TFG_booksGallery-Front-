29eae9bf3ddbf58ee2d2b8b66451fb7efa4f5172
Raul Mendez Poblete <82802107+Siname22@users.noreply.github.com>
2023-06-01 15:19:20 +0200

    cambios para el contenedor

---
+++ src/proxy.conf.js
@@ -0,0 +1,12 @@
+const PROXY_CONFIG = {
+  "/api": {
+    "target": process.env.BACKEND || "127.0.0.1:5000",
+    "secure": false,
+    "pathRewrite": {
+      "^/api": "/api"
+    },
+    "changeOrigin": true
+  }
+};
+
+module.exports = PROXY_CONFIG;
